acaad9a4895ccccfac6b5f642b0aadbcccf69a40
pernwn <pernillepjohansen@hotmail.com>
2023-11-01 23:49:00 +0100

    .

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -2,19 +2,6 @@ import splash from "../uniq.json"
 import Lottie from "lottie-react"
 import "../styles/vic.css"
 
-
-/*
-export function SplashAnimation() {splash.loadAnimation({
-    container: "div",
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '../uniq.json',
-  })
-
-}
-*/
-
 export const SplashAnimation = () => <Lottie animationData={splash} loop={true}/>
 
     
